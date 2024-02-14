148f34981a9c3eaa74aa9bc5cd05d5b5a9822bf1
tmaraki <toshiemaraki@gmail.com>
2024-02-13 21:45:20 -0500

    added routes folder and api and html js files to connect with server.js file. Coded server.js file

---
+++ Develop/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const html_routes = require('./routes/html-routes')
+const api_routes = require('./routes/api-routes')
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// express middleware 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
