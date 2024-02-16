853f98819dc17a318aece102b4fd07c6aa684a62
tmaraki <toshiemaraki@gmail.com>
2024-02-15 21:38:28 -0500

    Added root folder containing the starter code

--- Develop/server.js
+++ Develop/server.js
@@ -10,6 +10,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
+app.use(html_routes)
+app.use(api_routes)
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
